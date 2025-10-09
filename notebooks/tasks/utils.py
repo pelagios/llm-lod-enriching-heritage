6f02510f005b17c555f443356b21eb8fd8a3f345
eriktks <erikt@xs4all.nl>
2025-10-09 18:48:25 +0200

    first draft

--- notebooks/tasks/utils.py
+++ notebooks/tasks/utils.py
@@ -1,19 +1,15 @@
+from dotenv import load_dotenv
 import hashlib
 import importlib
 from IPython.display import clear_output, display
 import json
+import os
 import subprocess
 import sys
 try:
     from google.colab import files
 except:
     pass
-
-
-CHAR_PACKAGE = "📦"
-CHAR_SUCCESS = "✅"
-CHAR_FAILURE = "❌"
-COLORS = {"PERSON": "red", "LOCATION": "green", "OTHER": "blue"}
 
 
 def safe_import(package_name):
@@ -25,6 +21,15 @@ def safe_import(package_name):
         subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
         print(f"Finished installing {package_name}")
         return importlib.import_module(package_name)
+
+
+openai = safe_import("openai")
+
+
+CHAR_PACKAGE = "📦"
+CHAR_SUCCESS = "✅"
+CHAR_FAILURE = "❌"
+COLORS = {"PERSON": "red", "LOCATION": "green", "OTHER": "blue"}
 
 
 def squeal(text=None):
@@ -78,3 +83,62 @@ def save_results(texts, key, in_colab):
                 print(f"️{CHAR_FAILURE} Downloading preprocessed texts failed!")
         else:
             print(f"️{CHAR_SUCCESS} Saved preprocessed texts to file {output_file_name}")
+
+
+def extract_entities_from_ner_input(texts_input):
+    """For each entity in the input text return the entity text, context text and context text id""" 
+    return [{"entity_text": entity["text"],
+             "text_id": index,
+             "text": text["text_cleaned"]} 
+            for index, text in enumerate(texts_input) for entity in text["entities"]]
+
+
+def read_json_file(file_name):
+    """Read a json file and return its contents"""
+    with open(file_name, "r") as infile:
+        json_data = json.load(infile)
+        infile.close()
+    return json_data
+
+
+def write_json_file(file_name, json_data):
+    """Write json data to a file with the specified name"""
+    json_string = json.dumps(json_data, ensure_ascii=False, indent=2)
+    with open(file_name, "w") as outfile:
+        print(json_string, end="", file=outfile)
+        outfile.close()
+
+
+def get_openai_api_key():
+    """Extract OpenAI API key from environment or file and return it"""
+    load_dotenv()
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+    if not openai_api_key:
+        try:
+            with open("OPENAI_API_KEY", "r") as infile:
+                openai_api_key = infile.read().strip()
+                infile.close()
+        except:
+            pass
+    if not openai_api_key:
+        print(f"{utils.CHAR_FAILURE} no openai_api_key found!")
+        return ""
+    return openai_api_key
+
+
+def connect_to_openai(openai_api_key):
+    """Connect to OpenAI and return processing space"""
+    return openai.OpenAI(api_key=openai_api_key)
+
+
+def process_text_with_gpt(openai_client, model, prompt):
+    """Send text to OpenAI via prompt and return results"""
+    try:
+        response = openai_client.chat.completions.create(
+            model=model,
+            messages=[{"role": "user", "content": prompt}]
+        )
+        return response.choices[0].message.content
+    except:
+        print(f"{utils.CHAR_FAILURE} GPT call failed")
+        return []
